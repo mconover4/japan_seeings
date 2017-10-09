ac31c72ed7e66852c2d681973dee5c43eb488a6f
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-09 18:00:45 +0000

    done with users controller for now

--- japan-seeings/app/controllers/users_controller.rb
+++ japan-seeings/app/controllers/users_controller.rb
@@ -19,4 +19,40 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect '/spots'
+    end
+  end
+
+  post '/login' do
+    if logged_in?
+      redirect '/spots'
+    else
+      @user = User.find_by(:username => params[:username])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect '/spots'
+      else
+       redirect '/login'
+      end
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+
 end
