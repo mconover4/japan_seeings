4c63ac8b7679d52affbfecafc6380b4e311c45c9
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-09 17:54:15 +0000

    adjusted app controller

--- japan-seeings/app/controllers/application_controller.rb
+++ japan-seeings/app/controllers/application_controller.rb
@@ -5,10 +5,23 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
   get "/" do
-    erb :welcome
+    erb :home
+  end
+
+  helpers do
+
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+      !!current_user
+    end
   end
 
 end
